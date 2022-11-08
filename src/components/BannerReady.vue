2929b26e3e610847468ad1a0e742f60fa6f98d4c
kopatychxx <evgenijsalo88@gmail.com>
2022-11-08 18:49:13 +0100

    lending

--- src/components/BannerReady.vue
+++ src/components/BannerReady.vue
@@ -1,5 +1,28 @@
 <template>
   <div class="bannerReady">
+    <div class="bannerHeader">
+      <h3>ГОТОВІ КВАРТИРИ</h3>
+      <div class="subtitle">
+        <div class="lines">
+          <div class="line"></div>
+        </div>
+        <p style="color: gray">перші дві черги введенні в експлуатацію</p>
+      </div>
+    </div>
+    <div class="bannerContent">
+      <div class="left">
+        <p class="textDesktop">Комплекс з продуманою інфраструктурою в межах столиці та на природі одночасно. Тут є все, що може знадобитися, а метро та окружна дорога у 15-хвилинній доступності.
+          <br><br>
+          Життя з усіма її плюсами, але з незайманою природою за вікном - найкраще, що ви можете вибрати для себе та близьких.
+          <br><br>
+          Оберіть планування готових апартаментів від 20м2 до 115м2, замовте безкоштовний дизайн і насолоджуйтесь власним житлом вже сьогодні!
+        </p>
+        <p class="textMobile">Оберіть планування готових апартаментів від 20м2 до 115м2, замовте безкоштовний дизайн і насолоджуйтесь власним житлом вже сьогодні!</p>
+        <div class="butt">Записатися на перегляд
+        </div>
+      </div>
+      <img src="../assets/BannerReady.jpg" alt="">
+    </div>
   </div>
 </template>
 
@@ -12,6 +35,107 @@ export default {
 <style scoped>
 .bannerReady {
   display: flex;
+  flex-direction: column;
+  margin: 75px 15%;
 }
-
+.bannerHeader {
+  display: flex;
+  justify-content: space-between;
+  flex-direction: row;
+  align-items: flex-end;
+}
+.bannerHeader h3 {
+  font-size: 55px;
+}
+.subtitle{
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+}
+.line {
+  width: 50px;
+  height: 2px;
+  content: '';
+  background-color: green;
+  margin-right: 35px;
+}
+.bannerContent {
+  display: flex;
+  justify-content: space-between;
+  flex-direction: row;
+  margin-top: 55px;
+}
+.left {
+  width: 450px;
+}
+.bannerContent img{
+  height: 100%;
+  width: 450px;
+}
+.butt {
+  border: 1px solid green;
+  width: 200px;
+  text-align: center;
+  padding: 5px;
+  border-radius: 35px;
+  background: white;
+  cursor: pointer;
+  margin-top: 35px;
+}
+.butt:hover {
+  background: green;
+  transition: background 0.5s ease;
+  color: white;
+}
+.textMobile {
+  display: none;
+}
+@media screen and (max-width: 1024px) {
+  .bannerReady {
+    margin: 75px 5%;
+  }
+}
+@media screen and (max-width: 768px) {
+  .left {
+    width: 280px;
+  }
+  .bannerContent img {
+    width: 380px;
+  }
+  .bannerHeader h3 {
+    line-height: normal;
+  }
+}
+@media screen and (max-width: 425px) {
+  .bannerContent {
+    align-items: center;
+    flex-direction: column-reverse;
+  }
+  .bannerContent {
+    margin-top: 15px;
+  }
+  .bannerContent img {
+    width: 100%;
+  }
+  .textDesktop {
+    display: none;
+  }
+  .textMobile {
+    display: flex;
+  }
+  .left {
+    margin-top: 5px;
+    width: 100%;
+    text-align: center;
+  }
+  .butt {
+    margin: 25px auto;
+  }
+  .subtitle {
+    display: none;
+  }
+  .bannerHeader h3 {
+    font-size: 45px;
+  }
+}
 </style>
--- src/components/LandingComponent.vue
+++ src/components/LandingComponent.vue
@@ -1,6 +1,5 @@
 <template>
   <div class="landing">
-    <h1 class="landing__title">ЖИТЛОВИЙ КОМПЛЕКС <br> КОМФОРТ-КЛАСУ У БІЛОГОРОДЦІ</h1>
     <v-carousel class="carousel__mobile" cycle hide-delimiters :show-arrows="false" height="100vh">
       <v-carousel-item
           v-for="(item,i) in items"
@@ -10,6 +9,7 @@
     </v-carousel>
     <img class="landingImg" src="../assets/Landing/second.jpg" alt="">
     <div class="ar">
+      <h1 class="landing__title">ЖИТЛОВИЙ КОМПЛЕКС <br> КОМФОРТ-КЛАСУ У БІЛОГОРОДЦІ</h1>
       <div class="arrow-down">
         <span></span>
         <span></span>
@@ -50,11 +50,7 @@ export default {
   width: 100vw;
 }
 .landing__title {
-  z-index: 2;
   text-align: center;
-  position: absolute;
-  bottom: 15%;
-  left: 25%;
   font-weight: 400;
   font-size: 55px;
   line-height: 71px;
@@ -62,12 +58,15 @@ export default {
   text-shadow: 0 4px 4px rgba(0, 0, 0, 0.25);
 }
 .ar {
+  top: 700px;
+  position: absolute;
+  left: 30%;
+  z-index: 2;
   display: flex;
   justify-content: center;
+  flex-direction: column;
 }
 .arrow-down {
-  position: absolute;
-  bottom: 50px;
   margin: 0 auto;
 }
 .arrow-down span {
@@ -100,19 +99,20 @@ export default {
   }
 }
 @media screen and (max-width: 1440px) {
-  .landing__title {
+  .ar {
+    top: 400px;
     left: 20%;
   }
 }
 @media screen and (max-width: 1024px) {
-  .landing__title {
-    bottom: 20%;
+  .ar {
+    top: 400px;
     left: 7%;
   }
 }
 @media screen and (max-width: 768px) {
-  .landing__title {
-    left: 2%;
+  .ar {
+    top: 320px;
   }
   .carousel__mobile {
     display: block;
@@ -120,11 +120,9 @@ export default {
   .landingImg {
     display: none;
   }
-
 }
 @media screen and (max-width: 425px) {
   .landing__title {
-    bottom: 17%;
     font-size: 32px;
     line-height: normal;
   }
