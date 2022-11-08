dc519e1415464e4b27b557955a96894be648a128
kopatychxx <evgenijsalo88@gmail.com>
2022-11-08 15:41:44 +0100

    landing

--- src/components/LandingComponent.vue
+++ src/components/LandingComponent.vue
@@ -1,13 +1,79 @@
 <template>
-$END$
+  <div class="landing">
+    <h1 class="landing__title">ЖИТЛОВИЙ КОМПЛЕКС <br> КОМФОРТ-КЛАСУ У БІЛОГОРОДЦІ</h1>
+    <v-carousel cycle hide-delimiters :show-arrows="false" height="100vh">
+      <v-carousel-item
+          v-for="(item,i) in items"
+          :key="i"
+          :src="item.src"
+      ></v-carousel-item>
+    </v-carousel>
+  </div>
 </template>
 
 <script>
 export default {
-name: "LandingComponent"
+  name: "LandingComponent",
+  data () {
+    return {
+      items: [
+        {
+          src: require('../assets/Landing/first.jpg'),
+        },
+        {
+          src: require('../assets/Landing/second.jpg'),
+        }
+      ],
+    }
+  },
 }
 </script>
 
 <style scoped>
+.landing {
+  width: 100%;
+  height: 100%;
+}
+.landing__title {
+  z-index: 2;
+  text-align: center;
+  position: absolute;
+  top: 50%;
+  left: 25%;
+  font-weight: 400;
+  font-size: 55px;
+  line-height: 71px;
+  color: #ffffff;
+  text-shadow: 0 4px 4px rgba(0, 0, 0, 0.25);
+}
+@media screen and (max-width: 1440px) {
+  .landing__title {
+    left: 20%;
+  }
+}
+@media screen and (max-width: 1024px) {
+  .landing__title {
+    top: 40%;
+    left: 7%;
+  }
+}
+@media screen and (max-width: 768px) {
+  .landing__title {
+    left: 2%;
+  }
+}
+@media screen and (max-width: 425px) {
+  .landing__title {
+    top: 50%;
+    font-size: 32px;
+    line-height: normal;
+  }
+}
+@media screen and (max-width: 320px) {
+  .landing__title {
+    font-size: 25px;
+  }
+}
+
 
 </style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,14 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <LandingComponent />
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+  import LandingComponent from "@/components/LandingComponent";
 
-export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
+  export default {
+    name: 'HomeView',
+    components: {
+      LandingComponent
+    },
   }
-}
 </script>
